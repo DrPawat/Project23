579389c9dafde4dcb039bc4d1ab5378b80499cf1
PAWAT YAMWONG <64010646@KMITL.AC.TH>
2021-10-10 23:38:04 +0700

    Add project files.

---
+++ Project23/Source.cpp
@@ -0,0 +1,43 @@
+#include<stdio.h> 
+long permutation(int n, int r);
+long combination(int n, int r);
+long factorial(int num);
+
+int main(void)
+{
+    int n, r;
+
+    printf("Enter n: ");
+    scanf_s("%d", &n);
+
+    printf("Enter r: ");
+    scanf_s("%d", &r);
+
+    printf("Permutation = %ld\n", permutation(n, r));
+    printf("Combination = %ld", combination(n, r));
+
+    return 0; 
+}
+
+long permutation(int n, int r)
+{
+    return factorial(n) / factorial(n - r);
+}
+
+long combination(int n, int r)
+{
+    return permutation(n, r) / factorial(r);
+}
+
+long factorial(int num)
+{
+    long long fact = 1;
+
+    while (num > 0)
+    {
+        fact *= num;
+        num--;
+    }
+
+    return fact;
+}
